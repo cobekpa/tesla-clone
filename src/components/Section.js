799976bc32f0a5c624575584ca9ddf11e75ca33e
cobekpa <obekpacharles@yahoo.com>
2022-02-27 15:26:58 +0100

    Implemented reducers

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal'
+
 
 function Section({title, description, leftBtnText,  rightBtnText, backgroundImg}) {
   return (
     <Wrap bgImage = {backgroundImg}>
+      <Fade bottom>
       <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade> 
       <Buttons>
+        <Fade bottom>
           <ButtonGroup>
             <LeftButton>
                 {leftBtnText}
@@ -20,6 +25,7 @@ function Section({title, description, leftBtnText,  rightBtnText, backgroundImg}
             }
            
           </ButtonGroup>
+          </Fade>
           <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
     </Wrap>
